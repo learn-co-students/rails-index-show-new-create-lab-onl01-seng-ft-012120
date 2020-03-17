9641a8c39e75af47498766eb2635abf28dd4a54d
reddevilcero <reddevil_cero@hotmail.com>
2020-03-17 12:30:51 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,24 @@
+class CouponsController < ApplicationController
+  
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+  def new
+  end
+  
+  def create
+    @coupon = Coupon.new(coupon_code:params[:coupon][:coupon_code], store:params[:coupon][:store])
+    if @coupon.save
+      flash[:success] = "Coupon successfully created"
+      redirect_to @coupon
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+end
